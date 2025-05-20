f6434e82ab469623c0ee16d712557cf49d2f4fd4
gillwich <160880256+gillwich@users.noreply.github.com>
2025-05-20 11:50:51 +0200

    Projet 1

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 400);
+  background(255, 200, 90);
+}
+
+let taille = 1;
+let epaisseur = 0.3;
+
+function draw() {
+  noFill()
+  stroke(255, 90, 90)
+  strokeWeight(epaisseur)
+  circle(300, 400, taille);
+  taille = taille *1.3;
+  epaisseur = epaisseur *1.22;
+}
